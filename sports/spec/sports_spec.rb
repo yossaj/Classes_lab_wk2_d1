c15e0d768cb8470f88024b7f84a1d7a93762b304
yossaj <yossaj@icloud.com>
2019-04-08 20:31:15 +0100

    Just added new player

--- sports/spec/sports_spec.rb
+++ sports/spec/sports_spec.rb
@@ -7,7 +7,18 @@ class TestSports < MiniTest::Test
   def test_teams_name
     team = Team.new("The Sputters", ['Billy Bob', 'Billy Balls', 'Billy Band'], 'Old Bilbo')
     assert_equal("The Sputters", team.team_name)
+  end
 
+  def test_set_coaches_name
+    team = Team.new("The Sputters", ['Billy Bob', 'Billy Balls', 'Billy Band'], 'Old Bilbo')
+    team.coach=('New Bilbo')
+    assert_equal('New Bilbo', team.coach)
+  end
+
+  def test_add_player_to_team
+    team = Team.new("The Sputters", ['Billy Bob', 'Billy Balls', 'Billy Band'], 'Old Bilbo')
+    team.add_player_to_team('Billy Bongo')
+    assert_equal( 4,team.players.count)
   end
 
 
--- sports/sports.rb
+++ sports/sports.rb
@@ -1,5 +1,5 @@
 class Team
-  attr_reader :team_name, :players, :coach
+  attr_accessor :team_name, :players, :coach
 
   def initialize(team_name, players, coach)
     @team_name = team_name
@@ -18,4 +18,13 @@ class Team
   # def coach
   #   @coach
   # end
+  # def set_coaches_name(name)
+  #   @coach = name
+  # end
+
+  def add_player_to_team(new_player)
+    @players.push(new_player)
+  end
+
+
 end
