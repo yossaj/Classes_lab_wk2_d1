5b559f7d37d758b03a4988b2e2d0c9a01e30373e
yossaj <yossaj@icloud.com>
2019-04-08 16:07:04 +0100

    Part A finished?

--- class_lab.rb
+++ class_lab.rb
@@ -1,9 +1,10 @@
 class Student
-  attr_reader :student_name, :cohort
+  attr_reader :student_name, :cohort, :fav_language
 
-  def initialize(student_name, cohort)
+  def initialize(student_name, cohort, fav_language)
     @student_name = student_name
     @cohort = cohort
+    @fav_language = fav_language
   end
 
   # def student_name
@@ -17,8 +18,18 @@ class Student
   def set_cohort(num)
     @cohort = num
   end
+
   #
   # def cohort
   #   @cohort
   # end
+
+  def speech
+    return "I can talk!"
+  end
+
+  def say_favorite_language()
+    return "I love #{@fav_language}!"
+  end
+
 end
--- spec/class_lab_spec.rb
+++ spec/class_lab_spec.rb
@@ -5,19 +5,30 @@ require_relative('../class_lab')
 class TestStudent < MiniTest::Test
 
   def test_student_created
-    student = Student.new("Brian", "E31")
+    student = Student.new("Brian", "E31", "Ruby")
     assert_equal("Brian", student.student_name)
   end
 
   def test_set_student_name
-    student = Student.new("Tim", "E31")
+    student = Student.new("Tim", "E31", "Ruby")
     student.set_student_name('Tom')
     assert_equal('Tom', student.student_name)
   end
 
   def test_set_cohort
-    student = Student.new("Brian", "E21")
+    student = Student.new("Brian", "E21", "Ruby")
     student.set_cohort('E31')
     assert_equal('E31', student.cohort)
+  end
+
+  def test_speech
+    student = Student.new("Brian", "E21", "Ruby")
+    student.speech
+    assert_equal('I can talk!', student.speech)
+  end
+
+  def test_say_favorite_language
+    student = Student.new("Bill", "E41", "Ruby")
+    assert_equal('I love Ruby!', student.say_favorite_language)
   end
 end
