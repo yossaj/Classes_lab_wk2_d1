2dc0bddb4a557b8ddb27612df4acbc0cd364832c
yossaj <yossaj@icloud.com>
2019-04-08 21:31:14 +0100

    Think im finished with part B

--- sports/spec/sports_spec.rb
+++ sports/spec/sports_spec.rb
@@ -22,10 +22,22 @@ class TestSports < MiniTest::Test
   end
 
   def test_player_is_in_team
-    team = Team.newteam = Team.new("The Sputters", ['Billy Bob', 'Billy Balls', 'Billy Band', 'Billy Bongo'], 'Old Bilbo')
+    team = Team.new("The Sputters", ['Billy Bob', 'Billy Balls', 'Billy Band', 'Billy Bongo'], 'Old Bilbo')
     team.player_is_in_team?("Billy Bob")
-    assert_equal(true , true)
+    assert_equal(true , team.players[0] == 'Billy Bob')
   end
+
+  def test_team_score_default
+    team = Team.new("The Sputters", ['Billy Bob', 'Billy Balls', 'Billy Band', 'Billy Bongo'], 'Old Bilbo',)
+    assert_equal(0, team.team_score)
+  end
+
+  def test_team_score_add_points
+    team = Team.new("The Sputters", ['Billy Bob', 'Billy Balls', 'Billy Band', 'Billy Bongo'], 'Old Bilbo',)
+    team.team_win(true)
+    assert_equal(1,team.team_score)
+  end
+
 
 
 end
--- sports/sports.rb
+++ sports/sports.rb
@@ -1,10 +1,12 @@
 class Team
-  attr_accessor :team_name, :players, :coach
+  attr_accessor :team_name, :players, :coach, :team_score
 
-  def initialize(team_name, players, coach)
+  def initialize(team_name, players, coach, team_score = 0)
     @team_name = team_name
     @players = players
     @coach = coach
+    @team_score = team_score
+
   end
 
   # def team_name
@@ -28,6 +30,10 @@ class Team
 
   def player_is_in_team?(player)
     @players.include?(player)
+  end
+
+  def team_win(result)
+    @team_score += 1 if result
   end
 
 
